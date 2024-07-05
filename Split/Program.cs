67f2f5ef8797376e756034369175035870592b2f
shirley_m <shirley_m@DESKTOP-9FPOJ1O>
2024-07-05 17:12:30 +0300

    Add project files.

---
+++ Split/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> evens = new List<int>();
+            List<int> odds = new List<int>();
+
+            Console.Write("Up to how many numbers do you want to check?:");
+            int loops = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= loops; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+
+                    evens.Add(i);
+                }
+                else
+                {
+                    odds.Add(i);
+                }
+
+            }
+
+            Console.WriteLine("Printing even numbers:");
+
+            foreach (var item in evens)
+            { Console.Write($"{item} "); }
+
+            Console.WriteLine(Environment.NewLine + "Printing odd numbers:");
+            foreach (var item in odds)
+            { Console.Write($"{item} "); }
+
+            Console.ReadLine();
+            }
+        }
+    }
+
+
+
+
+
+
